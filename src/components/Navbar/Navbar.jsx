ea476aab084c28a3089cf6a29a11df3394717ccf
tushartyagi2003 <tushartyagi20122003@gmail.com>
2024-12-30 23:52:51 +0530

    nav updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,38 @@
 import React, {useState} from 'react'
 import styles from './Navbar.module.css';
-import {getImageUrl} from '../../utils'
+// import {getImageUrl} from '../../utils'
 
 const Navbar = () => {
-  console.log(getImageUrl("nav/menuIcon.png"));
 
-  return (
+  const [menuOpen, setMenuOpen] = useState(false)
+  
+  return ( <>
+  
     <nav className={styles.navbar}>
         <a className={styles.title} href="/">Portfolio</a>
 
         <div className={styles.menu}>
-          <img className={styles.menuBtn} src={getImageUrl("nav/closeIcon.png")} alt="tushar" />
-            <ul className={styles.menuItems}>
+          <img className={styles.menuBtn}
+           src={menuOpen ? 
+                "assets/nav/closeIcon.png" :
+                "assets/nav/menuIcon.png"}
+           alt="menuIcon" 
+           onClick={()=> setMenuOpen(!menuOpen)}/>
+
+         
+            <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+            onClick={() => setMenuOpen(false)}>
                 <li><a href="#about">About</a></li>
                 <li><a href="#experience">Experience</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
         </div>
+        
     </nav>
+
+    
+    </>
   )
 }
 
